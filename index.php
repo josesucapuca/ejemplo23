ba116d87c6a2731f71dd2ad6d7db5ccf5652f3e9
josesucapuca <josesucapuca@josemaster.lim.upeu.edu.pe>
2014-07-08 11:23:46 +0200

    aaaaaaa

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script  type="text/javascript"src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
+        <script src="apprise-1.5.full.js"type="text/javascript"></script>
+        <link rel="stylesheet" href="css/apprise.min.css" type="text/css">
+        
+        <script>
+
+</script>
+    <a id="mensaje" href="#mensaje.onclick" onclick="apprise('Abandonar el sitio e ir a GOOGLE',{'verify':true}, function(r)
+        {
+        if(r){
+           
+        }
+        else{
+            //el usuarioa a indicado no
+        }
+    });">Abandonar</a>
+    <a id="direccion"href="http://google.com" onclick="try">qqqq</a>
+
+        <!--<style>
+            #aaa{
+                ///display: none;
+                 background-color:blue;
+                 width: 200px;
+                 height: 200px;
+                 position: relative;
+                 float: left ;
+                 right: -50%; 
+                 top: -50%;
+            }
+        </style>-->
+    </head>
+    <body>
+       <!--<div id="hola" style="width:100px; height: 100px; background-color: yellow; position: relative" >
+            jajajajajjajaja
+            
+            
+        </div>
+        <div id="aaa">
+            asasdghkjhg
+            <button id="btn">Aceptar</button>
+        </div>-->
+    </body>
+</html>
